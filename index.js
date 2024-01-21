b8cce47313715f17240b5730248dfb99881079a2
alfaArghya <arghya.coding@gmail.com>
2024-01-22 01:39:54 +0530

    update main root

--- index.js
+++ index.js
@@ -4,7 +4,30 @@ const port = 3000;
 const leetcode = require("./leetCode")
 
 app.get("/", (req, res) => {
-    res.send(`<h3>localhost:3000/username</h3>`)
+    res.json({
+        routes: {
+            userDetails: {
+                "/:username": "get your profile Details",
+                "/:username/badges": "get your badges",
+                "/:username/solved": "get total number of question you solved",
+                "/:username/contest": "get your contest details",
+                "/:username/contest/history": "get all contest history",
+                "/:username/submission": "get your last 20 submission",
+                "/:username/submission?limit=7": "get some of your last submission",
+                "/:username/calendar": "get your submission calendar",
+            },
+            problems: {
+                dailyProblem: { "/daily": "get daily Problem", },
+                singleProblem: { "/select?titleSlug=two-sum": "get selected Problem", },
+                problemList: {
+                    "/problems": "get list of 20 problems",
+                    "/problems?limit=50": "get list of some problems",
+                    "/problems?tags=array+math": "get list problems on selected topics",
+                    "/problems?tags=array+math+string&limit=5": "get list some problems on selected topics",
+                },
+            }
+        }
+    })
 })
 
 app.get("/daily", leetcode.dailyProblem);
