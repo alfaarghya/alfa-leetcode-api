4c9f53b642ff7c469d16fbd7d1996b3aa1a97632
robo-mop <realaryanpingle@gmail.com>
2024-01-29 21:54:22 +0530

    fix: Enable CORS to make the API public

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const app = express();
 const port = 3000;
 const leetcode = require("./leetCode");
 
+// source: https://stackoverflow.com/a/18311469/8089674
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', false);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.get("/", (req, res) => {
   res.json({
     routes: {
