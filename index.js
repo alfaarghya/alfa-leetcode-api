9390a4a84ab9bf2a32da9e515229a2c4a42b6b15
alfaArghya <arghya.coding@gmail.com>
2024-02-12 01:05:47 +0530

    add CORS

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+const leetcode = require("./leetCode");
 const app = express();
 const port = 3000;
-const leetcode = require("./leetCode");
+
+app.use(cors()); //enable all CORS request
 
 app.get("/", (req, res) => {
   res.json({
