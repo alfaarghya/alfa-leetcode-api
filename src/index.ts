6f6506d25d8380299baf0ba248656f22372ccf41
alfaArghya <arghya.coding@gmail.com>
2024-06-02 21:31:26 +0530

    update starting log

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import app from './app';
 import config from './config';
 
 app.listen(config.port, () => {
-  console.log(`I am listening at http://localhost:${config.port}`);
+  console.log(`Server is running at => http://localhost:${config.port} ⚙️`);
 });
