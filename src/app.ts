c1e9776e52cc240cdde08906e1b937081571e469
alfaarghya <alfaarghya.dev@gmail.com>
2025-10-24 22:18:44 +0530

    increase API limit

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ let cache = apicache.middleware;
 
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour
-  limit: 60,
+  limit: 120,
   standardHeaders: 'draft-7',
   legacyHeaders: false,
   message: 'Too many request from this IP, try again in 1 hour',
